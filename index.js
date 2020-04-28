bd861964dcc1782ddf5c2e65bf1c63f5ed852535
harshthakur066 <harshthakur066@gmail.com>
2020-04-28 13:46:58 +0530

    connected to mongo DB

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const keys = require("./config/keys");
+
+require("./models/User");
 require("./services/passport");
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connenting to mongo", err);
+});
 
 const app = express();
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,10 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -9,8 +13,14 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accesToken) => {
-      console.log(accesToken);
+    async (accesToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (!existingUser) {
+        const newUser = await new User({ googleId: profile.id }).save();
+        done(null, newUser);
+      } else {
+        done(null, existingUser);
+      }
     }
   )
 );
