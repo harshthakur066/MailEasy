32d9b35e807d31b4dae0eac7c897486877e4c603
harshthakur066 <harshthakur066@gmail.com>
2020-05-13 16:10:30 +0530

    contet api setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { Provider as AuthProvider } from "../context/authContext";
+
 import Header from "./Header";
 
 const App = () => {
   return (
     <div className="container">
       <BrowserRouter>
-        <div>
+        <AuthProvider>
           <Header />
           {/* <Route exact path="/" component={} />
           <Route exact path="/surveys" component={} />
           <Route exact path="/surveys/news" component={} /> */}
-        </div>
+        </AuthProvider>
       </BrowserRouter>
     </div>
   );
