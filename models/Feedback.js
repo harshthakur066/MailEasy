e31b6b93c2c005bbcd3bfde4b8fdefc75df9d374
harshthakur066 <harshthakur066@gmail.com>
2020-05-16 02:11:04 +0530

    error fixed key added for sendgrid

--- models/Feedback.js
+++ models/Feedback.js
@@ -4,14 +4,14 @@ const customerSchema = require("./Customer");
 const { Schema } = mongoose;
 
 const feedbackSchema = new Schema({
-    _user: {type: Schema.Types.ObjectId, ref='User'},
+  _user: { type: Schema.Types.ObjectId, ref: "User" },
   title: String,
   subject: String,
   body: String,
   customers: [customerSchema],
   yes: { type: Number, default: 0 },
   no: { type: Number, default: 0 },
-  dateSent: Date
+  dateSent: Date,
 });
 
 mongoose.model("feedback", feedbackSchema);
