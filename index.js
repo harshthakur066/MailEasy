dff1e66bbed9c5243a0527d3156fb293e0e2645c
harshthakur066 <harshthakur066@gmail.com>
2020-05-16 21:32:46 +0530

    feedback related code removed from backend

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 
 require("./models/User");
-require("./models/Feedback");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI, {
